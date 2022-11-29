26cf2a7cb8225198f9848603db3df2b8b55f6c81
Nelson Vásquez <nelson.o.vasquez@gmail.com>
2022-11-29 15:48:32 -0600

    Cambios nelson

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
@@ -26,6 +27,9 @@ $app = new Laravel\Lumen\Application(
 $app->withFacades();
 
 $app->withEloquent();
+
+$app->register(\Barryvdh\DomPDF\ServiceProvider::class);
+$app->configure('dompdf');
 
 /*
 |--------------------------------------------------------------------------
